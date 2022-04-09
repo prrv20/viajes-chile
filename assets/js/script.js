2377c9466bcc76bf5446be57ca01ff51141136d1
Pedro <prrv20@gmail.com>
2022-04-08 21:30:32 -0400

    Detalles Finales, Documentación

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,5 @@
+/*Código para quitar la Transparencia al Navbar al hacer Scroll*/
 
-      /*var nav = document.querySelector('nav');
-
-      window.addEventListener('scroll', function () {
-        if (window.scrollY > 500) {
-          nav.classList.add('bg-secondary', 'shadow');
-          
-        } else {
-          nav.classList.remove('bg-secondary', 'shadow');
-        }
-      });*/
-     /* $(window).scroll(){
-          if($("#menu").offset().top > 500){
-              $("#menu").addClass("bg-success");
-          } else {
-            $("#menu").addRemove("bg-success");
-          }
-      }*/
       $(window).scroll(function(){
 
         if($("#menu").offset().top > 500){
